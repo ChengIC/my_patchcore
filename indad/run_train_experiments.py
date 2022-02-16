23e72bb5b86a651cd1c7f5f1b22cdc2efc77cde5
ChengIC <39056068+ChengIC@users.noreply.github.com>
2022-02-16 16:15:57 +0000

    run_feb16
    run_feb16

--- indad/run_train_experiments.py
+++ indad/run_train_experiments.py
@@ -3,7 +3,7 @@ from gen_config import GenConfig
 from train_patchcore import train_patchcore
 from test_patchcore import RunPatchcore
 from visual_patchcore import agvPatchCore
-
+import json
 
 
 if __name__ == "__main__":
@@ -14,7 +14,7 @@ if __name__ == "__main__":
     objs_imgs_folder='./datasets/full_body/test/objs'
 
     config0=GenConfig(config_dir,normal_imgs_folder,objs_imgs_folder)   
-    config_path_list = config0.genSemiConfig(percentage=2)
+    config_path_list = config0.genSemiConfig(percentage=20)
     
     for configPath in config_path_list:
         my_training = train_patchcore(configPath,normal_imgs_folder)
@@ -24,4 +24,9 @@ if __name__ == "__main__":
             'train_path':train_path
         }
     
+
+    json_filePath = 'train_log.json'
+    json_string = json.dumps(log_data)
+    with open(json_filePath, 'w') as outfile:
+        outfile.write(json_string)
 
