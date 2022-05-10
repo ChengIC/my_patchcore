3c84fc94f4e751364ab4c0aa8a9e46ae85bfa461
ChengIC <39056068+ChengIC@users.noreply.github.com>
2022-05-10 11:42:53 +0100

    arrange configs
    arrange configs

--- THzCore/myutils/config_utils.py
+++ THzCore/myutils/config_utils.py
@@ -15,190 +15,95 @@ def genTimeStamp():
     TimeStamp = time.strftime("%Y_%m_%d_%H_%M_%S", timeArray)
     return TimeStamp
 
+IMG_FORMATS = ['bmp', 'dng', 'jpeg', 'jpg', 'mpo', 'png', 'tif', 'tiff', 'webp']
+
 random.seed(20220329)
 
 class genConfig():
 
-    def __init__(self, timestring=None):
-        if timestring==None:
-            self.config_dir = os.path.join('./THzCore/Exp',genTimeStamp(),'config')
-        else:
-            self.config_dir = os.path.join('./THzCore/Exp',timestring,'config')
+    def __init__(self, 
+                config_parent_dir = './THzCore/Exp',
+                normal_imgs_folder=None,
+                timestring=None):
+
+        self.config_parent_dir=config_parent_dir
+        self.normal_imgs_folder=normal_imgs_folder
+        self.timestring=timestring
+        if self.timestring==None:
+            self.timestring=genTimeStamp()
+
+        # create config dir to store config files
+        self.config_dir = os.path.join(self.config_parent_dir,self.timestring,'config')
         if not os.path.exists(self.config_dir):
             os.makedirs(self.config_dir)
 
-    def genMultiConfig(self,config_idea='default',
-                normal_img_folder=None):
+    def genSingleConfig(self, 
+                        paras ={}):
 
-        set_scale=0.5
-        if config_idea == 'human_depended':
-            imgs_dict = {}
-            for img_file in os.listdir(normal_img_folder):      
-                human_model = img_file.split('_')[2]
-                if human_model not in imgs_dict:
-                    imgs_dict[human_model]=[]
-                imgs_dict[human_model].append(img_file)
-                
-            for human_model in imgs_dict:
-                config_data = {
-                    'img_ids':imgs_dict[human_model],
-                    'config_id':human_model + '_' + unique_id(8),
-                    'scale':set_scale,
-                }
-                config_data['info'] = "image scale: {}, config idea: {}, human model: {} ".format(config_data['scale'],config_idea,human_model )
-                
-                # save config data
-                json_file_name = config_data['config_id']+'.json'
-                json_filePath = os.path.join(self.config_dir, json_file_name)
-                json_string = json.dumps(config_data)
-                with open(json_filePath, 'w') as outfile:
-                    outfile.write(json_string)
+        # paras['bag_idx']
+        # paras['imgs_list']
+        # paras['config_id']
+        # paras['scale']
 
-        elif config_idea == 'shuffle_batch':
-            file_list = os.listdir(normal_img_folder)
-            random.shuffle(file_list)
-            img_ids = []
-            idx=0
-            for img_file in file_list:
-                img_ids.append(img_file)
-                if len(img_ids)>= int(0.1*len(file_list)):
-                    config_data = {
-                        'img_ids':img_ids,
-                        'config_id':'bacth_' + str(idx) + '_' + unique_id(8),
-                        'scale':set_scale,
-                        }
-                    config_data['info'] = "image scale: {}, config idea: {}, bacth idx: {} ".format(config_data['scale'],config_idea,idx)
+        verify_ids = []
+        for img_file in paras['imgs_list']:
+            if img_file.split('.')[-1] in IMG_FORMATS:
+                verify_ids.append(img_file)
 
-                    # save config data
-                    json_file_name = config_data['config_id']+'.json'
-                    json_filePath = os.path.join(self.config_dir, json_file_name)
-                    json_string = json.dumps(config_data)
-                    with open(json_filePath, 'w') as outfile:
-                        outfile.write(json_string)
-
-                    # reset image id and batch idx
-                    img_ids = []
-                    idx+=1
-        else:
-            pass
-
-        print ('Finish configs generation')
-        return self.config_dir
-
-    def bagging_config(self,num_sets=10,size_of_subset=None,normal_img_folder=None,
-                        bootstrap=True,set_scale=0.5):
-
-        file_list = os.listdir(normal_img_folder)
-        if size_of_subset==None:
-            size_of_subset = int(len(file_list)/num_sets)
-        if normal_img_folder:
-            if bootstrap:
-                for idx in range(num_sets):
-                    img_ids = random.sample(file_list,size_of_subset)
-                    config_data = {
-                        'img_ids':img_ids,
-                        'config_id':'bootstrap_bacth_' + str(idx) + '_' + unique_id(8),
-                        'scale':set_scale,
+        config_data = { 
+                        'img_ids':verify_ids,
+                        'config_id':'BagIdx_' + str(paras['bag_idx']) + '_' + 'Scale_' + str(paras['scale']) + '_' + paras['config_id'],
+                        'scale':paras['scale'],
                     }
-                    config_data['info'] = "image scale: {}, config idea: {}, bacth idx: {} num_sets: {}".format(config_data['scale'],'bootstrap',idx,num_sets)
 
-                    # save config data
-                    json_file_name = config_data['config_id']+'.json'
-                    json_filePath = os.path.join(self.config_dir, json_file_name)
-                    json_string = json.dumps(config_data)
-                    with open(json_filePath, 'w') as outfile:
-                        outfile.write(json_string)
+        # save config data
+        json_file_name = config_data['config_id']+'.json'
+        json_filePath = os.path.join(self.config_dir, json_file_name)
+        json_string = json.dumps(config_data)
+        with open(json_filePath, 'w') as outfile:
+            outfile.write(json_string)
 
-            else:
-                # no bootstrap equlats to non disjoint sampling
-                random.shuffle(file_list)
-                for idx in range(0,len(file_list),size_of_subset):
-                    img_ids = file_list[idx:idx+size_of_subset]
-                    if len(img_ids)<size_of_subset:
-                        continue
-                    config_data = {
-                        'img_ids':img_ids,
-                        'config_id':'disjoint_bacth_' + str(int(idx/size_of_subset)) + '_' + unique_id(8),
-                        'scale':set_scale,
-                    }
-                    config_data['info'] = "image scale: {}, config idea: {}, bacth idx: {} num_sets: {}".format(config_data['scale'],'disjoint',int(idx/size_of_subset),num_sets)
 
-                    # save config data
-                    json_file_name = config_data['config_id']+'.json'
-                    json_filePath = os.path.join(self.config_dir, json_file_name)
-                    json_string = json.dumps(config_data)
-                    with open(json_filePath, 'w') as outfile:
-                        outfile.write(json_string)
+    def genMultiConfigs(self,
+                        num_batch = 10, 
+                        imgs_per_bag=100,
+                        scale_step_size=10,
+                        bag_imgs_percentage=None, # 0.0 - 1.0
+                        method='default'             
+                    ):
+        if bag_imgs_percentage!=None:
+            selected_img_num = int(bag_imgs_percentage*len(self.normal_imgs_folder))
         else:
-            raise 'No image folder for training'
+            selected_img_num = imgs_per_bag
 
-        return self.config_dir
+        file_list = os.listdir(self.normal_imgs_folder)
+        scale_list = [i/100 for i in range(50,100,scale_step_size)] # scale setting: 0.5, 0.55, 0.6, 0.65 ... 1.0
 
-    def multiscale_config(self,
-                        bags_num=20,
-                        max_imgs_per_bag=100,
-                        scale_list = [0.5,0.6,0.7,0.8,0.9,1.0],
-                        normal_img_folder=None,):
-        file_list = os.listdir(normal_img_folder)
-        imgs_this_bag = max_imgs_per_bag
-        for s in scale_list:
-            imgs_this_bag = int(imgs_this_bag/2)+10
-            # print (imgs_this_bag)
-            for idx in range(bags_num):
-                img_ids = random.sample(file_list,imgs_this_bag)
-                config_data = {
-                    'img_ids':img_ids,
-                    'config_id':'bootstrap_bacth_scale_' + str(s) + '_'+ str(idx) + '_' + unique_id(8),
-                    'scale':s,
-                }
-                config_data['info'] = "image scale: {}, config idea: {}, imgs num: {}".format(config_data['scale'],'multi_scale_bootstrap',imgs_this_bag)
+        if 1 not in scale_list:
+            scale_list.append(1)
 
-                # save config data
-                json_file_name = config_data['config_id']+'.json'
-                json_filePath = os.path.join(self.config_dir, json_file_name)
-                json_string = json.dumps(config_data)
-                with open(json_filePath, 'w') as outfile:
-                    outfile.write(json_string)
-        
-        return self.config_dir
+        if method == 'default':
+            # perform multi-scale and multi-image bags
+            for i in range(num_batch):
+                imgs_list = random.sample(file_list,selected_img_num)
 
+                for s in scale_list:
+                    paras = {
+                        'imgs_list':imgs_list,
+                        'config_id':unique_id(8),
+                        'bag_idx':i,
+                        'scale':s,
+                    }
+                    self.genSingleConfig(paras)
 
-    def fix_imgs_multiscale(self,
-                            max_imgs_per_bag=100,
-                            normal_img_folder=None):
-        file_list = os.listdir(normal_img_folder)
-        imgs_this_bag = max_imgs_per_bag
-        img_ids = random.sample(file_list,imgs_this_bag)
-        scale_list = [i/100 for i in range(50,105,5)]
-        for s in scale_list:
-            config_data = {
-                    'img_ids':img_ids,
-                    'config_id':'bootstrap_bacth_scale_' + str(s) + '_' + unique_id(8),
-                    'scale':s,
-                }
-            config_data['info'] = "image scale: {}, config idea: {}, imgs num: {}".format(config_data['scale'],'multi_scale_bootstrap',imgs_this_bag)
-
-            # save config data
-            json_file_name = config_data['config_id']+'.json'
-            json_filePath = os.path.join(self.config_dir, json_file_name)
-            json_string = json.dumps(config_data)
-            with open(json_filePath, 'w') as outfile:
-                outfile.write(json_string)
 
         return self.config_dir
 
 
+if __name__ == "__main__":
+    normal_imgs_folder = './datasets/full_body/train/good'
+    config_dir = genConfig(normal_imgs_folder = normal_imgs_folder).genMultiConfigs(num_batch=10,imgs_per_bag=100)
 
-
-
-# if __name__ == "__main__":
-#     config = genConfig()
-# #   config_dir = config.bagging_config(normal_img_folder='./datasets/full_body/train/good',bootstrap=True)
-#     config_dir = genConfig().multiscale_config(
-#                         bags_num=20,
-#                         max_imgs_per_bag=400,
-#                         scale_list = [0.5,0.6,0.7,0.8,0.9,1.0],
-#                         normal_img_folder='./datasets/full_body/train/good')
 
 
     
