006a7be5a4be5dc46c9864f274e9ead8f20091e8
ChengIC <39056068+ChengIC@users.noreply.github.com>
2022-07-05 14:22:10 +0100

    Update twoStage.py

--- TwoStageCore/twoStage.py
+++ TwoStageCore/twoStage.py
@@ -14,7 +14,7 @@ class TwoStageCore():
         self.normal_img_dir=normal_img_dir
         self.obj_img_dir=obj_img_dir
     
-    def getPatchesImage (self, run_dir, img_dir, save_folder_name='cuts_1'):
+    def getPatchesImage (self, run_dir, img_dir, th=0, save_folder_name='cuts_1'):
 
         cutDir = os.path.join ('/'.join(self.FirstConifgDir.split('/')[:-1]), save_folder_name)
         if not os.path.exists(cutDir):
@@ -33,25 +33,26 @@ class TwoStageCore():
                     run_data = json.load(run_file)
                     detected_box_list = run_data['detected_box_list']
                     for selected_th in detected_box_list:
-                        detected_box = detected_box_list[selected_th]
-                        for bb in detected_box:
-                            crop = img[bb[1]:bb[3], bb[0]:bb[2]]
-                            crop_img_name = '{}_config_{}_crop_{}.jpg'.format(img_id, config_id, unique_id(12))
-                            
-                            save_crop_path = os.path.join(cutDir, crop_img_name)
-                            cv2.imwrite(save_crop_path, crop)
+                        if selected_th > th:
+                            detected_box = detected_box_list[selected_th]
+                            for bb in detected_box:
+                                crop = img[bb[1]:bb[3], bb[0]:bb[2]]
+                                crop_img_name = '{}_config_{}_crop_{}.jpg'.format(img_id, config_id, unique_id(12))
+                                
+                                save_crop_path = os.path.join(cutDir, crop_img_name)
+                                cv2.imwrite(save_crop_path, crop)
 
-                            exp_info ={
-                                    'img_id':img_id,
-                                    'save_crop_path':crop_img_name,
-                                    'bbox':bb,
-                            }
+                                exp_info ={
+                                        'img_id':img_id,
+                                        'save_crop_path':crop_img_name,
+                                        'bbox':bb,
+                                }
 
-                            json_string = json.dumps(exp_info)
-                            crop_json_name = crop_img_name.replace('jpg','json')
-                            crop_json_path = os.path.join(cutDir, crop_json_name)
-                            with open(crop_json_path, 'w') as outfile:
-                                outfile.write(json_string)
+                                json_string = json.dumps(exp_info)
+                                crop_json_name = crop_img_name.replace('jpg','json')
+                                crop_json_path = os.path.join(cutDir, crop_json_name)
+                                with open(crop_json_path, 'w') as outfile:
+                                    outfile.write(json_string)
 
         print('finish cropping and return cut dir')
         return cutDir
@@ -79,11 +80,15 @@ class TwoStageCore():
         self.SecondModelDir = TrainPatchCore(self.SecondConifgDir, save_name='models_2').trainModel()
 
 
-    # def inference(self):
-    #     self.SecondRunDir =  InferenceCore(self.FirstModelDir,save_name='runs_2').inference_one_model(self.obj_img_dir)
+    def inference(self):
+        self.SecondRunDir =  InferenceCore(self.FirstModelDir,save_name='runs_2').inference_one_model(self.obj_img_dir)
 
+        self.SecondCutDir = self.getPatchesImage(run_dir=self.SecondRunDir,
+                                                    img_dir=self.obj_img_dir,
+                                                    th = 0.7,
+                                                    save_folder_name='cuts_2')
 
-
+        self.FinalRunDir = InferenceCore(self.SecondModelDir,save_name='final_runs').inference_one_model(self.SecondCutDir)
 
 
 if __name__ == "__main__":
@@ -92,3 +97,4 @@ if __name__ == "__main__":
 
     tS = TwoStageCore(normal_img_dir=normal_img_dir,obj_img_dir=obj_img_dir)
     tS.train()
+    tS.inference()
