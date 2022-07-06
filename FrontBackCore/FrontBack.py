ebc8b785292d69c6bfb4ebfa6775d565bd06d4e7
ChengIC <39056068+ChengIC@users.noreply.github.com>
2022-07-06 16:53:33 +0100

    add front and back

---
+++ FrontBackCore/FrontBack.py
@@ -0,0 +1,74 @@
+
+from train_utils import *
+from inference_utils import *
+from ran_utils import *
+import random
+import json
+from tqdm import tqdm
+
+random.seed(19940308)
+
+# generate two configurations front and back
+
+def genConfigFile(exp_dir, img_dir, scale=1, info='front', num_of_imgs=10):
+
+    config_dir = os.path.join(exp_dir, 'config_' + info)
+   
+    if not os.path.exists(config_dir):
+        os.makedirs(config_dir)
+    
+    qualified_list = []
+    for img_file in os.listdir(img_dir):
+        if info in img_file:
+            qualified_list.append(img_file)
+    
+    selected_list = random.choices(qualified_list, k=num_of_imgs)
+
+    config_data = {}
+    config_data['filename'] = '{}_{}.json'.format(unique_id(12), info)
+    config_data['img_folder'] = img_dir
+    config_data['img_ids'] = selected_list
+    config_data['scale'] = scale
+
+    json_string = json.dumps(config_data)
+    json_file_path = os.path.join(config_dir, config_data['filename'])
+    with open(json_file_path, 'w') as outfile:
+        outfile.write(json_string)
+
+    return config_dir
+
+
+
+# unit test
+if __name__ == "__main__":
+    exp_dir = './FrontBackCore/exp/' + genTimeStamp()
+    img_dir = './datasets/full_body/train/good'
+
+    ############
+    ##### training 
+    ############
+    config_dir1 = genConfigFile(exp_dir, img_dir, scale=1, info='front', num_of_imgs=1)
+    config_dir2 = genConfigFile(exp_dir, img_dir, scale=1, info='back', num_of_imgs=1)
+    
+    for config_dir in [config_dir1, config_dir2]:
+        model_dir = TrainPatchCore(config_dir).trainModel()
+        print ('finished training model of {}'.format(model_dir))
+
+
+    ############
+    ##### inference  
+    ############
+    obj_dir = './datasets/full_body/test/objs'
+    ### model_dir = './FrontBackCore/exp/2022_07_06_16_42_45/models'
+    for single_model_dir in os.listdir(model_dir):
+        model_path = os.path.join(model_dir, single_model_dir)
+        info = single_model_dir.split('_')[-1]
+        
+        if os.path.isdir(model_path):
+            print ('loading model from {}'.format(model_path))
+            run_core = InferenceCore(model_path)
+            # inference img dir
+            for img_file in tqdm(os.listdir(obj_dir)):
+                if info in img_file:
+                    result = run_core.inference_one_img(os.path.join(obj_dir,img_file))
+                    print (result)
--- FrontBackCore/inference_utils.py
+++ FrontBackCore/inference_utils.py
@@ -10,160 +10,74 @@ import os
 from PIL import Image
 from torchvision import transforms
 
+
+
+def load_model_from_dir(model_path):
+    state_dict_path = os.path.join(model_path,'Core_Path')
+    tar_path = os.path.join(model_path,'Core_Path.tar')
+    configPath =os.path.join(model_path,'loader_info.json')
+
+    # load model and config
+    model = PatchCore(f_coreset=.10, backbone_name="wide_resnet50_2")
+    if torch.cuda.is_available():
+        model.load_state_dict(torch.load(state_dict_path))
+        model_paras = torch.load(tar_path)
+    else:
+        model.load_state_dict(torch.load(state_dict_path,map_location ='cpu'))
+        model_paras = torch.load(tar_path,map_location ='cpu')
+    with open(configPath) as json_file:
+        config_data = json.load(json_file)
+
+    print ('loading model dir: ' + model_path + ' sucessfully')
+
+    return model, model_paras, config_data
+
+
+def load_image_to_tensor_cpu(img_path, config_data):
+
+    IMAGENET_MEAN = tensor([.485, .456, .406])
+    IMAGENET_STD = tensor([.229, .224, .225])
+    transfoms_paras = [
+            transforms.ToTensor(),
+            transforms.Normalize(IMAGENET_MEAN, IMAGENET_STD),
+        ]
+    if config_data['scale']!=1:
+        transfoms_paras.append(transforms.Resize(config_data['resize_box'], interpolation=transforms.InterpolationMode.BICUBIC))
+    loader = transforms.Compose(transfoms_paras)
+
+    image = Image.open(img_path).convert('RGB')
+    original_size_width, original_size_height = image.size
+    image = loader(image).unsqueeze(0)
+
+    test_img_tensor = image.to('cpu', torch.float)
+    HeatMap_Size = [original_size_height, original_size_width]
+
+    return test_img_tensor, HeatMap_Size
+
+
 class InferenceCore():
 
-    def __init__(self, model_dir=None, save_name='runs') :
-        self.model_dir = model_dir
-        self.run_dir = os.path.join ('/'.join(self.model_dir.split('/')[:-1]), save_name)
+    def __init__(self, model_path=None, save_name='runs') :
+        self.model_path = model_path
+        self.run_dir = os.path.join ('/'.join(self.model_path.split('/')[:-2]), save_name)
         if not os.path.exists(self.run_dir):
             os.makedirs(self.run_dir)
 
-    def load_model_from_dir(self, model_path):
-        state_dict_path = os.path.join(model_path,'Core_Path')
-        tar_path = os.path.join(model_path,'Core_Path.tar')
-        configPath =os.path.join(model_path,'loader_info.json')
+        # begin loading patch-core from model path
+        self.model, self.model_paras, self.config_data = load_model_from_dir(model_path)
 
-        # load model and config
-        model = PatchCore(f_coreset=.10, backbone_name="wide_resnet50_2")
-        if torch.cuda.is_available():
-            model.load_state_dict(torch.load(state_dict_path))
-            model_paras = torch.load(tar_path)
-        else:
-            model.load_state_dict(torch.load(state_dict_path,map_location ='cpu'))
-            model_paras = torch.load(tar_path,map_location ='cpu')
-        with open(configPath) as json_file:
-            config_data = json.load(json_file)
-
-        print ('loading model dir: ' + model_path + ' sucessfully')
-
-        return model, model_paras, config_data
-
-    def load_image_to_tensor_cpu(self, img_path, config_data):
-
-        IMAGENET_MEAN = tensor([.485, .456, .406])
-        IMAGENET_STD = tensor([.229, .224, .225])
-        transfoms_paras = [
-                transforms.ToTensor(),
-                transforms.Normalize(IMAGENET_MEAN, IMAGENET_STD),
-            ]
-
-        if config_data['scale']!=1:
-            transfoms_paras.append(transforms.Resize(config_data['resize_box'], interpolation=transforms.InterpolationMode.BICUBIC))
-
-        loader = transforms.Compose(transfoms_paras)
-
-        image = Image.open(img_path).convert('RGB')
-        original_size_width, original_size_height = image.size
-        image = loader(image).unsqueeze(0)
-
-        test_img_tensor = image.to('cpu', torch.float)
-        HeatMap_Size = [original_size_height, original_size_width]
-
-        return test_img_tensor, HeatMap_Size
-
-
-    def inference_one_img(self, model_path, img_path):
+    def inference_one_img(self, img_path):
         # for testing and insight
-        model, model_paras, config_data = self.load_model_from_dir(model_path)
-        test_img_tensor, HeatMap_Size = self.load_image_to_tensor_cpu(img_path, config_data)
-        img_lvl_anom_score, pxl_lvl_anom_score = model.inference (test_img_tensor, model_paras, HeatMap_Size)
+        
+        test_img_tensor, HeatMap_Size = load_image_to_tensor_cpu(img_path, self.config_data)
+        img_lvl_anom_score, pxl_lvl_anom_score = self.model.inference (test_img_tensor, self.model_paras, HeatMap_Size)
         detected_box_list = PixelScore2Boxes(pxl_lvl_anom_score)
 
         result  = {
+            'img_path': img_path,
             'img_score':img_lvl_anom_score,
-            'pixel_score':pxl_lvl_anom_score,
+            # 'pixel_score':pxl_lvl_anom_score,
             'bbox':detected_box_list,
         }
 
         return result
-
-    # inference patches
-    def inference_one_model (self, img_dir):
-        for single_model_dir in os.listdir(self.model_dir):
-            model_path = os.path.join(self.model_dir, single_model_dir)
-
-            # load model
-            if os.path.isdir(model_path):
-                model, model_paras, config_data = self.load_model_from_dir(model_path)
-
-                # inference img one by one
-                for img_file in tqdm(os.listdir(img_dir)):
-                    if img_file.split('.')[-1] in IMG_FORMATS:
-                        img_path = os.path.join(img_dir, img_file)
-                        test_img_tensor, HeatMap_Size = self.load_image_to_tensor_cpu(img_path, config_data)
-                        img_lvl_anom_score, pxl_lvl_anom_score = model.inference (test_img_tensor, model_paras, HeatMap_Size)
-                        detected_box_list = PixelScore2Boxes(pxl_lvl_anom_score)
-
-                        # log exp
-                        img_id = img_file.split('/')[-1].split('.')[0]
-                        json_file_name = img_id + '_config_' + single_model_dir + '.json'
-                        json_filePath = os.path.join(self.run_dir, json_file_name)
-                        
-                        exp_info ={
-                            'img_path':img_path,
-                            'detected_box_list':detected_box_list,
-                            'model_dir':single_model_dir,
-                            'img_id':img_id,
-                            'img_score':img_lvl_anom_score.tolist(),
-                            # 'pixel_score':pxl_lvl_anom_score.tolist(),
-                        }
-
-                        json_string = json.dumps(exp_info)
-                        with open(json_filePath, 'w') as outfile:
-                            outfile.write(json_string)
-
-        return self.run_dir
-    
-    # continues inference
-    def continuous_inference(self, img_dir):
-        inference_files_list = os.listdir(self.run_dir).copy()
-
-        for single_model_dir in tqdm(os.listdir(self.model_dir)):
-            model_path = os.path.join(self.model_dir, single_model_dir)
-
-            if os.path.isdir(model_path):
-                model, model_paras, config_data = self.load_model_from_dir(model_path)
-
-                for img_file in tqdm(os.listdir(img_dir)):
-                    img_id = img_file.split('/')[-1].split('.')[0]
-                    json_file_name = img_id + '_config_' + single_model_dir + '.json'
-
-                    if json_file_name in inference_files_list:
-                        print ('already inference')
-
-                    else:
-                        print ('load model from {} to inference image {}'.format(model_path, img_file))
-                        if img_file.split('.')[-1] in IMG_FORMATS:
-                            img_path = os.path.join(img_dir, img_file)
-                            test_img_tensor, HeatMap_Size = self.load_image_to_tensor_cpu(img_path, config_data)
-                            img_lvl_anom_score, pxl_lvl_anom_score = model.inference (test_img_tensor, model_paras, HeatMap_Size)
-                            detected_box_list = PixelScore2Boxes(pxl_lvl_anom_score)
-
-                            # log exp
-                            img_id = img_file.split('/')[-1].split('.')[0]
-                            json_file_name = img_id + '_config_' + single_model_dir + '.json'
-                            json_filePath = os.path.join(self.run_dir, json_file_name)
-                            
-                            exp_info ={
-                                'img_path':img_path,
-                                'detected_box_list':detected_box_list,
-                                'model_dir':single_model_dir,
-                                'img_id':img_id,
-                                'img_score':img_lvl_anom_score.tolist(),
-                               # 'pixel_score':pxl_lvl_anom_score.tolist(),
-                            }
-
-                            json_string = json.dumps(exp_info)
-                            with open(json_filePath, 'w') as outfile:
-                                outfile.write(json_string)
-
-        return self.run_dir
-
-
-if __name__ == "__main__":
-    print ('test image inference')
-    ### generate configure files
-    img_dir = './datasets/full_body/test/objs'
-    model_dir = './TwoStageCore/exp/2022_07_01_10_55_40/models'
-    run_dir =  InferenceCore(model_dir).inference_one_model(img_dir)
-    print (run_dir)
